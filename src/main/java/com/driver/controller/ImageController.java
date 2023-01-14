fcb30c07deb90f52b95fee053ade37784342f76e
Deepanshigreza <deepanshigreza@gmail.com>
2023-01-14 23:16:01 +0530

    done

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -2,32 +2,44 @@ package com.driver.controller;
 
 import com.driver.models.Blog;
 import com.driver.models.Image;
+import com.driver.repositories.BlogRepository;
+import com.driver.repositories.ImageRepository;
 import com.driver.services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.font.ImageGraphicAttribute;
+import java.util.List;
+
 @RestController
 @RequestMapping("/images")
 public class ImageController {
+
+    @Autowired
+    ImageService imageService;
+
+    @Autowired
+    private ImageRepository imageRepository;
 
     @PostMapping("/create")
     public ResponseEntity<Image> createAndReturn(@RequestBody Blog blog,
                                                  @RequestParam String description,
                                                  @RequestParam String dimensions) {
-        Image image = null;
+        Image image = imageService.createAndReturn(blog, description, dimensions);
         return new ResponseEntity<>(image, HttpStatus.CREATED);
     }
 
     @GetMapping("/countImagesInScreen/{id}/{screenDimensions}")
     public ResponseEntity<Integer> countImagesInScreen(@PathVariable int id, @PathVariable String screenDimensions){
-        int count = 0;
+        int count = imageService.countImagesInScreen(imageRepository.findById(id).get(), screenDimensions);
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteImage(@PathVariable int id) {
+        imageService.deleteImage(imageRepository.findById(id).get());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -26,27 +26,54 @@ public class BlogService {
 
     public List<Blog> showBlogs(){
         //find all blogs
-
+        List<Blog> blogList=blogRepository1.findAll();
+        return blogList;
     }
 
     public void createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
 
-        //updating the blog details
+        User user=userRepository1.findById(userId).get();
 
-        //Updating the userInformation and changing its blogs
+        Blog blog=new Blog(title,content);
+
+        blog.setUser(user);
+
+        List<Blog> blogList=user.getBlogList();
+        blogList.add(blog);
+        user.setBlogList(blogList);
+
+        userRepository1.save(user);
 
     }
 
     public Blog findBlogById(int blogId){
         //find a blog
+        Blog blog = blogRepository1.findById(blogId).get(); //by me
+        return blog;
     }
 
     public void addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog after creating it
+        Blog blog=blogRepository1.findById(blogId).get();
+
+        Image image=imageService1.createAndReturn(blog,description,dimensions);
+
+        image.setBlog(blog);
+
+        List<Image> imageList=blog.getImageList();
+        if(imageList==null) imageList=new ArrayList<>();
+        imageList.add(image);
+        blog.setImageList(imageList);
+
+        blogRepository1.save(blog);
+
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        if(blogRepository1.findById(blogId).isPresent()){
+            blogRepository1.deleteById(blogId);
+        }
     }
 }
